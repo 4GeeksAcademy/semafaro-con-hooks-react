029c1397d130ed0ef08d1d76884478caea71dc2a
Amanda Dias <168840059+mnddevl@users.noreply.github.com>
2024-08-05 12:10:15 +0000

    boton actualizado

--- src/js/component/trafficLight.jsx
+++ src/js/component/trafficLight.jsx
@@ -1,29 +1,36 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 const TrafficLight = () => {
-	const [ color, setColor] = useState("red");
-	return (
-	<div className="padre"> 
-	<div className="palo"> </div>
- <div className="container">
-            <div 
-                className={`semafaro ${color === 'red' ? 'rojo activo' : 'red'}`} 
-                onClick={() => setColor('red')}
-            ></div>
-            <div 
-                className={`semafaro ${color === 'yellow' ? 'amarillo activo' : 'yellow'}`} 
-                onClick={() => setColor('yellow')}
-            ></div>
-            <div 
-                className={`semafaro ${color === 'green' ? 'verde activo' : 'green'}`} 
-                onClick={() => setColor('green')}
-            ></div>
-			<div 
-                className={`semafaro ${color === 'purple' ? 'morado activo' : 'purple'}`} 
-                onClick={() => setColor('purple')}
-            ></div>
+    const [color, setColor] = useState("red");
+
+    const botonMorado = () => {
+        setColor("purple");
+    };
+
+    return (
+        <div className="padre">
+            <div className="palo"></div>
+            <div className="container">
+                <div 
+                    className={`semafaro ${color === 'red' ? 'rojo activo' : 'rojo'}`} 
+                    onClick={() => setColor('red')}
+                ></div>
+                <div 
+                    className={`semafaro ${color === 'yellow' ? 'amarillo activo' : 'amarillo'}`} 
+                    onClick={() => setColor('yellow')}
+                ></div>
+                <div 
+                    className={`semafaro ${color === 'green' ? 'verde activo' : 'verde'}`} 
+                    onClick={() => setColor('green')}
+                ></div>
+                <div 
+                    className={`semafaro ${color === 'purple' ? 'morado activo' : 'morado'}`} 
+                    onClick={() => setColor('purple')}
+                ></div>
+            </div>
+            <button onClick={botonMorado}>Encender luz morada</button>
         </div>
-		</div>
     );
 };
+
 export default TrafficLight;
